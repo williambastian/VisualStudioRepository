e34dc9a8a6ab3587f3987a67fad43efe3abf4e80
William <87213388+williambastian@users.noreply.github.com>
2022-11-21 15:05:31 -0600

    first commit

---
+++ WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function newText() {
+    document.getElementById("hello_world").innerHTML = "&#128131; Hello World &#128131;";
+}
+
+function oldText() {
+    document.getElementById("hello_world").innerHTML = "Hello World";
+}
